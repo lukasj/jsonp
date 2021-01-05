98464fe9bdfde45e5933a2b96c4f8858a3eb8978
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-01-05 13:06:10 +0100

    remove unnecessary/empty methods
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- impl/src/main/java/org/glassfish/json/JsonObjectBuilderImpl.java
+++ impl/src/main/java/org/glassfish/json/JsonObjectBuilderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -320,21 +320,6 @@ class JsonObjectBuilderImpl implements JsonObjectBuilder {
         @Override
         public JsonObject asJsonObject() {
             return this;
-        }
-
-        @Override
-        public int size() {
-            return valueMap.size();
-        }
-
-        @Override
-        public JsonValue get(Object key) {
-            return valueMap.get(key);
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            return valueMap.containsKey(key);
         }
     }
 
--- impl/src/test/java/org/glassfish/json/tests/JsonObjectTest.java
+++ impl/src/test/java/org/glassfish/json/tests/JsonObjectTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -31,9 +31,6 @@ import jakarta.json.*;
 public class JsonObjectTest extends TestCase {
     public JsonObjectTest(String testName) {
         super(testName);
-    }
-
-    public void test() {
     }
 
     public void testEmptyObjectEquals() throws Exception {
